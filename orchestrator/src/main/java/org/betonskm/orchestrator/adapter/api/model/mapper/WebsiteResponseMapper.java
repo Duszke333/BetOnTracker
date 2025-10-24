4f839f67f6c1b40f72ca9fd33ed8767c8a49ffe6
Duszke333 <duszke333@gmail.com>
2025-10-24 21:41:52 +0200

    New response just dropped

--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/model/mapper/WebsiteResponseMapper.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/model/mapper/WebsiteResponseMapper.java
@@ -1,5 +1,7 @@
 package org.betonskm.orchestrator.adapter.api.model.mapper;
 
+import java.util.List;
+import org.betonskm.orchestrator.adapter.api.model.response.FetchWebsitesAPIResponse;
 import org.betonskm.orchestrator.adapter.api.model.response.WebsiteAPIResponse;
 import org.betonskm.orchestrator.configuration.MapperConfiguration;
 import org.betonskm.orchestrator.domain.website.Website;
@@ -9,4 +11,12 @@ import org.mapstruct.Mapper;
 public interface WebsiteResponseMapper {
 
   WebsiteAPIResponse toAPIResponse(Website website);
+
+  FetchWebsitesAPIResponse.WebsiteResponse toWebsiteResponse(Website website);
+
+  List<FetchWebsitesAPIResponse.WebsiteResponse> toWebsiteResponses(List<Website> websites);
+
+  default FetchWebsitesAPIResponse toFetchWebsitesAPIResponse(List<Website> websites) {
+    return new FetchWebsitesAPIResponse(toWebsiteResponses(websites));
+  }
 }
