9a1f42966e0a5f2b0643158c8d13042dc213ef49
Duszke333 <duszke333@gmail.com>
2025-10-24 20:24:13 +0200

    new usecase

--- orchestrator/src/main/java/org/betonskm/orchestrator/application/port/in/CategoryManagementUseCase.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/application/port/in/CategoryManagementUseCase.java
@@ -1,9 +1,11 @@
 package org.betonskm.orchestrator.application.port.in;
 
 import java.util.List;
+import org.betonskm.orchestrator.application.command.AddWebsiteToCategoryCommand;
 import org.betonskm.orchestrator.application.command.CreateCategoryCommand;
 import org.betonskm.orchestrator.application.command.DecommissionCategoryCommand;
 import org.betonskm.orchestrator.domain.category.Category;
+import org.betonskm.orchestrator.domain.website.Website;
 
 public interface CategoryManagementUseCase {
 
@@ -12,4 +14,6 @@ public interface CategoryManagementUseCase {
   List<Category> fetchAllActiveCategories();
 
   void decommissionCategory(DecommissionCategoryCommand command);
+
+  Website addWebsiteToCategory(AddWebsiteToCategoryCommand command);
 }
--- orchestrator/src/main/java/org/betonskm/orchestrator/application/service/CategoryManagementService.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/application/service/CategoryManagementService.java
@@ -2,15 +2,21 @@ package org.betonskm.orchestrator.application.service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.betonskm.orchestrator.application.command.AddWebsiteToCategoryCommand;
 import org.betonskm.orchestrator.application.command.CreateCategoryCommand;
 import org.betonskm.orchestrator.application.command.DecommissionCategoryCommand;
 import org.betonskm.orchestrator.application.port.in.CategoryManagementUseCase;
 import org.betonskm.orchestrator.application.port.out.CategoryRepository;
+import org.betonskm.orchestrator.application.port.out.CategoryWebsiteRepository;
+import org.betonskm.orchestrator.application.port.out.WebsiteRepository;
 import org.betonskm.orchestrator.configuration.TimeProvider;
 import org.betonskm.orchestrator.configuration.exception.OrchestratorException;
 import org.betonskm.orchestrator.domain.category.Category;
+import org.betonskm.orchestrator.domain.website.Website;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +27,8 @@ public class CategoryManagementService implements CategoryManagementUseCase {
 
   private final CategoryRepository categoryRepository;
   private final TimeProvider timeProvider;
+  private final WebsiteRepository websiteRepository;
+  private final CategoryWebsiteRepository categoryWebsiteRepository;
 
 
   @Override
@@ -56,5 +64,34 @@ public class CategoryManagementService implements CategoryManagementUseCase {
     category.setDecommissionedAt(timeProvider.now());
     categoryRepository.save(category);
     log.info("[CATEGORY MANAGEMENT] Decommissioned category: {}", category);
+  }
+
+  @Override
+  @Transactional
+  @Modifying
+  public Website addWebsiteToCategory(AddWebsiteToCategoryCommand command) {
+
+    if (categoryRepository.fetchCategoryById(command.getCategoryId()).isEmpty()) {
+      throw new OrchestratorException("Category with ID " + command.getCategoryId() + " not found");
+    }
+
+    Website savedWebsite;
+    Optional<Website> optionalWebsite = websiteRepository.fetchByUrl(command.getWebsiteUrl());
+    if (optionalWebsite.isPresent()) {
+      Website website = optionalWebsite.get();
+      log.info("[CATEGORY MANAGEMENT] Website with URL {} already exists: {}", command.getWebsiteUrl(), website);
+      website.incrementReferenceCount();
+      savedWebsite = websiteRepository.save(website);
+    } else {
+      Website website = Website.builder()
+          .url(command.getWebsiteUrl())
+          .build();
+      savedWebsite = websiteRepository.save(website);
+      log.info("[CATEGORY MANAGEMENT] Created new website: {}", savedWebsite);
+    }
+
+    categoryWebsiteRepository.link(command.getCategoryId(), savedWebsite.getId());
+    log.info("[CATEGORY MANAGEMENT] Added website to category: {}", savedWebsite);
+    return savedWebsite;
   }
 }
