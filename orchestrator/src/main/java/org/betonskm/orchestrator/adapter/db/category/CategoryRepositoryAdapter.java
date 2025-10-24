f7ade05f573f80796029f99966a09b4eaaeb912d
Duszke333 <duszke333@gmail.com>
2025-10-24 17:18:25 +0200

    Fetch all active categories endpoint

--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/category/CategoryRepositoryAdapter.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/category/CategoryRepositoryAdapter.java
@@ -1,5 +1,6 @@
 package org.betonskm.orchestrator.adapter.db.category;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.betonskm.orchestrator.application.port.out.CategoryRepository;
@@ -17,5 +18,10 @@ public class CategoryRepositoryAdapter implements CategoryRepository {
   @Override
   public Category save(Category category) {
     return mapper.fromEntity(repository.save(mapper.toEntity(category)));
+  }
+
+  @Override
+  public List<Category> fetchAllActiveCategories() {
+    return repository.findAllActiveCategories().stream().map(mapper::fromEntity).toList();
   }
 }
