46aea04ea457d2bfe370904c038c516ad5e76418
Duszke333 <duszke333@gmail.com>
2025-10-24 19:46:10 +0200

    Add new repository

--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/categoryWebsite/CategoryWebsiteEntityId.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/categoryWebsite/CategoryWebsiteEntityId.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class CategoryWebsiteId implements Serializable {
+public class CategoryWebsiteEntityId implements Serializable {
 
   @Serial
   private static final long serialVersionUID = 2622231438285495094L;
--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/website/WebsiteEntityRepository.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/website/WebsiteEntityRepository.java
@@ -2,7 +2,9 @@ package org.betonskm.orchestrator.adapter.db.website;
 
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WebsiteEntityRepository extends JpaRepository<WebsiteEntity, UUID> {
 
 }
