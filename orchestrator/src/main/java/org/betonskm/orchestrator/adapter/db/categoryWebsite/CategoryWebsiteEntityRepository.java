1762a4461e04453cbe09ceff5f4759a400b9a744
Duszke333 <duszke333@gmail.com>
2025-10-24 20:58:51 +0200

    Cleanup

--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/categoryWebsite/CategoryWebsiteEntityRepository.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/categoryWebsite/CategoryWebsiteEntityRepository.java
@@ -1,19 +1,9 @@
 package org.betonskm.orchestrator.adapter.db.categoryWebsite;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryWebsiteEntityRepository extends JpaRepository<CategoryWebsite, CategoryWebsiteId> {
 
-  List<CategoryWebsite> findByCategoryId(Long categoryId);
-
-  List<CategoryWebsite> findByWebsiteId(UUID websiteId);
-
-  Optional<CategoryWebsite> findByCategoryIdAndWebsiteId(Long categoryId, UUID websiteId);
-
-  void deleteByCategoryIdAndWebsiteId(Long categoryId, UUID websiteId);
 }
