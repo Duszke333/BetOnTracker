403661924111e96974f7a17e0d6b6d81813dc15a
Duszke333 <duszke333@gmail.com>
2025-10-24 17:18:25 +0200

    Fetch all active categories endpoint

--- orchestrator/src/main/java/org/betonskm/orchestrator/application/port/out/CategoryRepository.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/application/port/out/CategoryRepository.java
@@ -1,8 +1,11 @@
 package org.betonskm.orchestrator.application.port.out;
 
+import java.util.List;
 import org.betonskm.orchestrator.domain.category.Category;
 
 public interface CategoryRepository {
 
   Category save(Category category);
+
+  List<Category> fetchAllActiveCategories();
 }
