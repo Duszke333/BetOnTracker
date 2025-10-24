a3d3001b69183054008692ddc78f74cb3a80b452
Duszke333 <duszke333@gmail.com>
2025-10-24 16:55:44 +0200

    Naming convention change

--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/model/mapper/CategoryResponseMapper.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/model/mapper/CategoryResponseMapper.java
@@ -1,15 +1,15 @@
 package org.betonskm.orchestrator.adapter.api.model.mapper;
 
-import org.betonskm.orchestrator.adapter.api.model.response.CreateCategoryAPIResponse;
+import org.betonskm.orchestrator.adapter.api.model.response.CategoryAPIResponse;
 import org.betonskm.orchestrator.configuration.MapperConfiguration;
 import org.betonskm.orchestrator.domain.category.Category;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfiguration.class)
-public interface CreateCategoryResponseMapper {
+public interface CategoryResponseMapper {
 
   @Mapping(target = "categoryId", source = "id")
   @Mapping(target = "categoryName", source = "name")
-  CreateCategoryAPIResponse toAPIResponse(Category category);
+  CategoryAPIResponse toAPIResponse(Category category);
 }
