0d356bcb1d81916d7b8fed48e732c984cc48f89a
Duszke333 <duszke333@gmail.com>
2025-10-24 21:53:33 +0200

    Endpoint for fetching all active websites

--- orchestrator/src/main/java/org/betonskm/orchestrator/application/port/out/WebsiteRepository.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/application/port/out/WebsiteRepository.java
@@ -1,5 +1,6 @@
 package org.betonskm.orchestrator.application.port.out;
 
+import java.util.List;
 import java.util.Optional;
 import org.betonskm.orchestrator.domain.website.Website;
 
@@ -7,4 +8,6 @@ public interface WebsiteRepository {
 
   Website save(Website website);
   Optional<Website> fetchByUrl(String url);
+
+  List<Website> fetchActiveWebsites();
 }
