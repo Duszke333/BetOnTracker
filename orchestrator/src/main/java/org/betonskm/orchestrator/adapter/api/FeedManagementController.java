2815849bad82da308432a23c5d7b187085276881
Duszke333 <duszke333@gmail.com>
2025-10-24 16:48:31 +0200

    Wire it all together

--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/FeedManagementController.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/FeedManagementController.java
@@ -8,8 +8,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.betonskm.orchestrator.adapter.api.model.mapper.CreateCategoryResponseMapper;
 import org.betonskm.orchestrator.adapter.api.model.request.CreateCategoryAPIRequest;
+import org.betonskm.orchestrator.adapter.api.model.response.CreateCategoryAPIResponse;
+import org.betonskm.orchestrator.application.command.CreateCategoryCommand;
+import org.betonskm.orchestrator.application.port.in.CategoryManagementUseCase;
 import org.betonskm.orchestrator.configuration.annotations.DefaultApiSecurity;
+import org.betonskm.orchestrator.domain.category.Category;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,10 +29,18 @@ public class FeedManagementController {
 
   private static final String CREATE_FEED_PATH = FEED_MANAGEMENT_PREFIX + "/create";
 
+  private final CategoryManagementUseCase categoryManagementUseCase;
+  private final CreateCategoryResponseMapper createCategoryResponseMapper;
+
   @PostMapping(path = CREATE_FEED_PATH, consumes = APPLICATION_JSON_VALUE)
   @DefaultApiSecurity
-  @ApiResponse(responseCode = "201", description = "Feed category created successfully")
-  public void createCategory(@RequestBody @Valid CreateCategoryAPIRequest createCategoryAPIRequest) {
+  @ApiResponse(responseCode = "200", description = "Feed category created successfully")
+  public CreateCategoryAPIResponse createCategory(@RequestBody @Valid CreateCategoryAPIRequest createCategoryAPIRequest) {
     log.info("[CREATE CATEGORY] Request received: {}", createCategoryAPIRequest);
+
+    CreateCategoryCommand command = CreateCategoryCommand.from(createCategoryAPIRequest.getCategoryName());
+    Category createdCategory = categoryManagementUseCase.createCategory(command);
+
+    return createCategoryResponseMapper.toAPIResponse(createdCategory);
   }
 }
