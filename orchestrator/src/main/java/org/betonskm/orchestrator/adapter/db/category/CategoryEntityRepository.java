285a0a60d46a4848360b64fdd60b4a432f8fa109
Duszke333 <duszke333@gmail.com>
2025-10-24 17:44:24 +0200

    Fix wrong ID type

--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/category/CategoryEntityRepository.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/category/CategoryEntityRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CategoryEntityRepository extends JpaRepository<CategoryEntity, String> {
+public interface CategoryEntityRepository extends JpaRepository<CategoryEntity, Integer> {
 
   @Query("FROM CategoryEntity c WHERE c.decommissionedAt IS NULL")
   List<CategoryEntity> findAllActiveCategories();
