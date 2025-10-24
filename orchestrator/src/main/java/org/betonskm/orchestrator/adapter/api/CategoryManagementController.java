1fdc80ce80bd0c9f91f3223a6532d9aedd0822ad
Duszke333 <duszke333@gmail.com>
2025-10-24 20:56:34 +0200

    Wire it

--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/CategoryManagementController.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/CategoryManagementController.java
@@ -13,14 +13,19 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.betonskm.orchestrator.adapter.api.model.mapper.CategoryResponseMapper;
+import org.betonskm.orchestrator.adapter.api.model.mapper.WebsiteResponseMapper;
+import org.betonskm.orchestrator.adapter.api.model.request.AddWebsiteToFeedAPIRequest;
 import org.betonskm.orchestrator.adapter.api.model.request.CreateCategoryAPIRequest;
 import org.betonskm.orchestrator.adapter.api.model.response.CategoryAPIResponse;
 import org.betonskm.orchestrator.adapter.api.model.response.FetchAllCategoriesAPIResponse;
+import org.betonskm.orchestrator.adapter.api.model.response.WebsiteAPIResponse;
+import org.betonskm.orchestrator.application.command.AddWebsiteToCategoryCommand;
 import org.betonskm.orchestrator.application.command.CreateCategoryCommand;
 import org.betonskm.orchestrator.application.command.DecommissionCategoryCommand;
 import org.betonskm.orchestrator.application.port.in.CategoryManagementUseCase;
 import org.betonskm.orchestrator.configuration.annotations.DefaultApiSecurity;
 import org.betonskm.orchestrator.domain.category.Category;
+import org.betonskm.orchestrator.domain.website.Website;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,9 +44,11 @@ public class CategoryManagementController {
   private static final String CREATE_CATEGORY_PATH = FEED_MANAGEMENT_PREFIX + "/categories/create";
   private static final String FETCH_CATEGORIES_PATH = FEED_MANAGEMENT_PREFIX + "/categories/fetch";
   private static final String DECOMISSION_CATEGORY_PATH = FEED_MANAGEMENT_PREFIX + "/categories/{categoryId}/delete";
+  private static final String ADD_WEBSITE_TO_FEED_PATH = FEED_MANAGEMENT_PREFIX + "/categories/{categoryId}/add";
 
   private final CategoryManagementUseCase categoryManagementUseCase;
   private final CategoryResponseMapper categoryResponseMapper;
+  private final WebsiteResponseMapper websiteResponseMapper;
 
   @PostMapping(path = CREATE_CATEGORY_PATH, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
   @DefaultApiSecurity
@@ -82,5 +89,21 @@ public class CategoryManagementController {
 
     DecommissionCategoryCommand command = DecommissionCategoryCommand.from(categoryId);
     categoryManagementUseCase.decommissionCategory(command);
+  }
+
+  @PostMapping(path = ADD_WEBSITE_TO_FEED_PATH, consumes = APPLICATION_JSON_VALUE)
+  public WebsiteAPIResponse addWebsiteToFeed(
+      @Parameter(description = "ID of the category to which the website will be added", required = true, example = "1")
+      @PathVariable("categoryId") @Positive Integer categoryId,
+      @RequestBody @Valid AddWebsiteToFeedAPIRequest request
+  ) {
+    log.info("[ADD WEBSITE TO FEED] Request received for categoryId: {} to add website: {}", categoryId, request);
+
+    AddWebsiteToCategoryCommand command = AddWebsiteToCategoryCommand.from(categoryId, request.getWebsiteUrl());
+    // Implementation goes here
+
+    Website addedWebsite = categoryManagementUseCase.addWebsiteToCategory(command);
+
+    return websiteResponseMapper.toAPIResponse(addedWebsite);
   }
 }
---
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/model/response/WebsiteAPIResponse.java
@@ -0,0 +1,9 @@
+package org.betonskm.orchestrator.adapter.api.model.response;
+
+import java.util.UUID;
+
+public class WebsiteAPIResponse {
+
+  private UUID id;
+  private String url;
+}
