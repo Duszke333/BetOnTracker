9bbe1cb464c950c96f84834eb72c067a655efe60
Duszke333 <duszke333@gmail.com>
2025-10-24 13:41:15 +0200

    Add APIRequest for creating a feed

---
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/api/model/request/CreateCategoryAPIRequest.java
@@ -0,0 +1,34 @@
+package org.betonskm.orchestrator.adapter.api.model.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.betonskm.orchestrator.adapter.api.model.validation.ValidUUID;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateCategoryAPIRequest {
+
+  @Schema(
+      requiredMode = RequiredMode.REQUIRED,
+      description = "ID of the user owning the category",
+      example = "df884722-eef6-4601-9313-2d5d8e4524b7"
+  )
+  @NotBlank
+  @ValidUUID
+  private String userId;
+
+  @Schema(
+      requiredMode = RequiredMode.REQUIRED,
+      description = "Name of the feed associated with the category",
+      example = "Tech News"
+  )
+  @NotBlank
+  private String feedName;
+}
