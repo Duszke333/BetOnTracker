b2d85fe3fe5d8a99a5b151b2b9f50af5260ab2cc
Piotr Jabłoński <penciller@disroot.org>
2025-10-24 19:57:37 +0200

    add tags

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,13 +1,19 @@
-export interface Item {
+export interface RssTag {
+  id: string;
+  name: string;
+}
+
+export interface RssItem {
   id: string;
   title: string;
   link: string;
   description: string;
+  tags: RssTag[];
   pubDate: Date;
 }
 
-export interface Channel {
+export interface RssChannel {
   title: string;
   description: string;
-  items: Item[];
+  items: RssItem[];
 }
