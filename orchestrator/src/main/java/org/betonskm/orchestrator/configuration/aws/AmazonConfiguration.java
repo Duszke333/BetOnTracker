5cebaf303aac7a3f84bb58d9044a8d724731baa2
Duszke333 <duszke333@gmail.com>
2025-10-24 22:14:40 +0200

    Add SQS configuration

---
+++ orchestrator/src/main/java/org/betonskm/orchestrator/configuration/aws/AmazonConfiguration.java
@@ -0,0 +1,52 @@
+package org.betonskm.orchestrator.configuration.aws;
+
+import static org.apache.commons.lang3.ObjectUtils.allNotNull;
+
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.awscore.client.builder.AwsClientBuilder;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(AmazonProperties.class)
+public class AmazonConfiguration {
+
+  private final AmazonProperties amazonProperties;
+
+  @Bean
+  public SqsAsyncClient sqsAsyncClient() {
+    return apply(amazonProperties.getSqs().getEndpoint(), SqsAsyncClient.builder())
+        .region(Region.of(amazonProperties.getSqs().getRegion()))
+        .credentialsProvider(buildCredentialsProvider())
+        .build();
+  }
+
+  private AwsCredentialsProvider buildCredentialsProvider() {
+    if (allNotNull(amazonProperties.getAccessKeyId(), amazonProperties.getSecretKey())) {
+      return () -> AwsBasicCredentials.create(
+          amazonProperties.getAccessKeyId(),
+          amazonProperties.getSecretKey()
+      );
+    } else {
+      return DefaultCredentialsProvider.create();
+    }
+  }
+
+  private <T extends AwsClientBuilder<T, C>, C> AwsClientBuilder<T, C> apply(String endpoint, AwsClientBuilder<T, C> builder) {
+    if (StringUtils.isNotEmpty(endpoint)) {
+      builder.endpointOverride(URI.create(endpoint));
+    }
+    return builder;
+  }
+}
