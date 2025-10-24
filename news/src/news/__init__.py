0f4d8e9b6704aafa51f8d7cca5deb1f336e066da
maksnowak <59841779+maksnowak@users.noreply.github.com>
2025-10-24 18:22:33 +0200

    Add S3 upload

--- news/src/news/__init__.py
+++ news/src/news/__init__.py
@@ -1,7 +1,11 @@
-from . import feeds
+from . import feeds, upload_to_s3
 import logging
 
 def main() -> None:
     logging.basicConfig(level=logging.INFO)
     logging.info("News module initialized.")
-    feeds.parse_feed('https://feedparser.readthedocs.io/en/latest/examples/rss20.xml')
+    entries, _, _ = feeds.parse_feed('https://feedparser.readthedocs.io/en/latest/examples/rss20.xml')
+    for entry in entries:
+        content = f"Title: {entry['title']}\nSummary: {entry['summary'].value}\n"
+        upload_to_s3.upload_to_s3(content)
+    logging.info("News module processing completed.")
