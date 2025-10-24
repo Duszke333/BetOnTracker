47b276a8a69e94da9421383f492263c11833b4a5
Duszke333 <duszke333@gmail.com>
2025-10-24 17:18:25 +0200

    Fetch all active categories endpoint

--- orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/category/CategoryEntityRepository.java
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/db/category/CategoryEntityRepository.java
@@ -1,7 +1,11 @@
 package org.betonskm.orchestrator.adapter.db.category;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoryEntityRepository extends JpaRepository<CategoryEntity, String> {
 
+  @Query("FROM CategoryEntity c WHERE c.decommissionedAt IS NULL")
+  List<CategoryEntity> findAllActiveCategories();
 }
