3e12241f8b99c382509b4fbb34ca04dfe08aa5d4
Duszke333 <duszke333@gmail.com>
2025-10-25 03:00:47 +0200

    Add Article summary SQS listener

---
+++ orchestrator/src/main/java/org/betonskm/orchestrator/adapter/event/listener/summary/model/ArticleSummaryEvent.java
@@ -0,0 +1,25 @@
+package org.betonskm.orchestrator.adapter.event.listener.summary.model;
+
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleSummaryEvent {
+
+  private UUID articleId;
+  private String articlePath;
+  private String oneLineSummary;
+  private List<String> keywords;
+  private Short importanceScore;
+  private Short sentimentScore;
+  private Short sourceReliabilityScore;
+}
